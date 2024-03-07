45f70294928cc86fe878810608538e6a6e99a99f
Gabriel Felipe <gabrielfelipe.oliveira2000@gmail.com>
2024-03-07 14:02:04 -0300

    Deletando pastas antigas.

--- portfolio/onebit/3-modulo/1-classe.py
+++
@@ -1,7 +0,0 @@
-# Criando uma classe | A classe é só o modelo, para ter um objeto precisamos instaciar a classe
-
-class Movie: # Usamos a primeira letra maiúscula para nomear uma clase
-    nome = "" # -> Atributos da classe Movie
-    ano_de_lancamento = 0
-    duracao = 0
-    nota = 0
